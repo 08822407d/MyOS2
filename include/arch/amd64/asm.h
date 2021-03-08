5cc71b683cc83a3750b36f2b98359b800ddb7f9d
08822407d <08822407d@gmail.com>
2021-03-08 16:57:37 +0800

    adjust cmakelists

--- include/arch/amd64/asm.h
+++ include/arch/amd64/asm.h
@@ -2,17 +2,19 @@
 #define  _AMD64_ASM_H_   
 
     #ifdef __ELF__
-    #   define _C_LABEL(x)	x
+    #   define _C_LABEL(x)	x 
+    #   define _C_LABEL_LABEL(x)  x:
     #else
     #   ifdef __STDC__
     #       define _C_LABEL(x)	_ ## x
+    #       define _C_LABEL_LABEL(x)   _ ## x:
     #   else
     #       define _C_LABEL(x)	_/**/x
+    #       define _C_LABEL_LABEL(x)   _/**/x:
     #   endif
     #endif
     #define	_ASM_LABEL(x)	x
 
-    #define _C_LABEL_LABEL(x)   _ ## x:
     #define ENTRY(name)             \
             .global _C_LABEL(name); \
             _C_LABEL_LABEL(name)
