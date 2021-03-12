8daf7e094b385a8daac07a115b8158226e2008aa
08822407d <08822407d@gmail.com>
2021-03-12 17:22:57 +0800

    backup

--- kernel/arch/amd64/include/archtypes.h
+++ kernel/arch/amd64/include/archtypes.h
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/cdefs.h>
 
+/* type field of segdesc, for codeseg and dataseg */
 typedef enum
 {
 	// R=read, W=write, E=executable, A=accessed
@@ -17,6 +18,7 @@ typedef enum
 	ER_CODE		= 10,
 	ERA_CODE	= 11,
 } CommSegType;
+/* type field of segdesc, for system segment */
 typedef enum
 {	
 	LDTdesc		= 2,
@@ -27,26 +29,56 @@ typedef enum
 	TRAPGATE	= 15
 } SysSegType;
 
-typedef struct __attribute__((packed)) {/* segment descriptor for protected mode */
-	uint16_t			: 16;
-	uint32_t			: 24;
+/* 64bit version segdesc for common segment */
+typedef struct __attribute__((packed)) {
+	uint16_t			: 16; // unused
+	uint32_t			: 24; // unused
 	uint8_t		Type	: 4;
 	uint8_t		Sflag	: 1; // 0=system seg, 1=common seg
 	uint8_t		Privil	: 2;
 	uint8_t		Present	: 1;
-	uint8_t				: 6;
+	uint8_t				: 6; // unused
 	uint8_t 	Lflag	: 1; // 0=dataseg, 1=codeseg
-	uint16_t			: 9;
+	uint16_t			: 9; // unused
 } segdesc64_s;
 
+/* 64bit version segdesc for TSS segment */
+typedef struct  __attribute__((packed)) {
+	uint16_t	Limit1	: 16;
+	uint32_t	Base1	: 24;
+	uint8_t		Type	: 4;
+	uint8_t				: 1; // Sflag must be 0
+	uint8_t		DPL		: 2;
+	uint8_t		Present	: 1;
+	uint8_t		Limit2	: 4;
+	uint8_t		AVL		: 1;
+	uint8_t				: 3; // unused
+	uint64_t	Base2	: 40;
+	uint32_t			: 32; // unused
+} TSSsegdesc;
+
+/* 64bit version segdesc for gate */
 typedef struct __attribute__((packed)) {
-	uint16_t	leng	: 16;
-	uint16_t	base1	: 16;
-	uint32_t	attr	: 32;
-	uint32_t	base2	: 32;
-	uint32_t			: 32;
+	uint16_t	offs1	: 16;
+	uint16_t	segslct	: 16;
+	uint8_t		IST		: 3;
+	uint8_t				: 5; // unused
+	uint8_t		Type	: 4;
+	uint8_t				: 1; // Sflag must be 0
+	uint8_t		DPL		: 2;
+	uint8_t		Present	: 1;
+	uint64_t	offs	: 48;
+	uint32_t			: 32; // unused
 } gatedesc64_s;
 
+/* desc selector */
+typedef struct __attribute__((packed)) {
+	uint8_t		RPL		: 2;
+	uint8_t				: 1; // TIflag we always set it to 0
+	uint16_t	Index	: 13;
+} descselector_s;
+
+/* input for 64bit version instruction lgdt and lidt */
 typedef struct __attribute__((packed)) {
 	uint32_t	limit;
 	segdesc64_s	*base;
@@ -55,13 +87,13 @@ typedef struct __attribute__((packed)) {
 typedef struct {
 } segframe_s;
 
-/* protect.c */
+/* 64bit version TSS */
 typedef struct __attribute__((packed)) {
-	uint32_t		: 32;
+	uint32_t		: 32; // unused
 	uint64_t rsp0	: 64;
 	uint64_t rsp1	: 64;
 	uint64_t rsp2	: 64;
-	uint64_t		: 64;
+	uint64_t		: 64; // unused
 	uint64_t ist1	: 64;
 	uint64_t ist2	: 64;
 	uint64_t ist3	: 64;
@@ -69,8 +101,8 @@ typedef struct __attribute__((packed)) {
 	uint64_t ist5	: 64;
 	uint64_t ist6	: 64;
 	uint64_t ist7	: 64;
-	uint64_t		: 64;
-	uint32_t		: 32;
+	uint64_t		: 64; // unused
+	uint32_t		: 32; // unused
 	uint32_t iobm	: 32;
 } tss64_s;
 
--- kernel/arch/amd64/protect.c
+++ kernel/arch/amd64/protect.c
@@ -8,38 +8,41 @@
 
 /* Storage for gdt, idt and tss. */
 segdesc64_s gdt[GDT_SIZE] __aligned(SEGDESC_SIZE);
+descselector_s gdtslctr[GDT_SIZE] __aligned(SEGDESC_SIZE);
 gatedesc64_s idt[IDT_SIZE] __aligned(GATEDESC_SIZE);
 tss64_s tss[CONFIG_MAX_CPUS];
 desctblptr64_s gdt_ptr;
 desctblptr64_s idt_ptr;
 
-void set_codeseg(uint32_t index, CommSegType type, uint8_t privil)
+void set_commseg(uint32_t index, CommSegType type, uint8_t privil, uint8_t Lflag)
 {
 	segdesc64_s *sd = &(gdt[index]);
-	// fixed bits for codeseg
-	sd->Lflag	= 1;
 	sd->Sflag	= 1;
 	sd->Present	= 1;
 	// changable bits
+	sd->Lflag	= Lflag;
 	sd->Type	= type;
 	sd->Privil	= privil;
+
+	descselector_s *ds = &(gdtslctr[index]);
+	ds->Index	= index;
+	ds->RPL		= privil;
+}
+
+void set_codeseg(uint32_t index, CommSegType type, uint8_t privil)
+{
+	set_commseg(index, type, privil, 1);
 }
 
 void set_dataseg(uint32_t index, CommSegType type, uint8_t privil)
 {
-	segdesc64_s *sd = &(gdt[index]);
-	// fixed bits for dataseg
-	sd->Lflag	= 0;
-	sd->Sflag	= 1;
-	sd->Present	= 1;
-	// changable bits
-	sd->Type	= type;
-	sd->Privil	= privil;
-}
+	set_commseg(index, type, privil, 0);
+}	
 
 void init_gdt()
 {
 	memset(gdt, 0, sizeof(gdt));
+	memset(gdtslctr, 0, sizeof(gdtslctr));
 
 	// gdt[0] has been set to 0 by memset
 	set_codeseg(KERN_CS_INDEX, E_CODE, 0);
@@ -64,23 +67,17 @@ void prot_init(void)
 	init_idt();
 
 
-	__asm__ __volatile__("lgdt %0": : "m"(gdt_ptr));
+	__asm__ __volatile__("	lgdt	%0			\n\
+							mov		%1, %%ds	\n\
+							mov		%1, %%es	\n\
+							mov		%1, %%fs	\n\
+							mov		%1, %%gs	\n\
+							mov		%1, %%ss	\n	"
+						 :
+						 :	"m"(gdt_ptr),
+						 	"a"(gdtslctr[KERN_DS_INDEX]));
+
 	__asm__ __volatile__("lidt %0": : "m"(idt_ptr));
-
-	// /* Build GDT */
-	// init_param_dataseg(&gdt[LDT_INDEX],
-	// (phys_bytes) 0, 0, INTR_PRIVILEGE); /* unusable LDT */
-	// gdt[LDT_INDEX].access = PRESENT | LDT;
-	// init_codeseg(KERN_CS_INDEX, INTR_PRIVILEGE);
-	// init_dataseg(KERN_DS_INDEX, INTR_PRIVILEGE);
-	// init_codeseg(USER_CS_INDEX, USER_PRIVILEGE);
-	// init_dataseg(USER_DS_INDEX, USER_PRIVILEGE);
-
-	// /* Currently the multiboot segments are loaded; which is fine, but
-	// * let's replace them with the ones from our own GDT so we test
-	// * right away whether they work as expected.
-	// */
-	// prot_load_selectors();
 
 	// /* Set up a new post-relocate bootstrap pagetable so that
 	// * we can map in VM, and we no longer rely on pre-relocated
