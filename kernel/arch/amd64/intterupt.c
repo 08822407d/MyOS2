1f831857030c4d872926dc24bb7df1095250d0e4
08822407d <08822407d@gmail.com>
2021-03-29 09:59:24 +0800

    excep and intr handler now can work

--- kernel/arch/amd64/intterupt.c
+++ kernel/arch/amd64/intterupt.c
@@ -8,14 +8,69 @@ extern char *(intr_name[IDT_SIZE]);
 
 void excep_inval_tss(stack_frame_s * sf)
 {
-	color_printk(RED, BLACK, "Err Code : sele-%d, TI-%d, IDT-%d, EXT-%d \n",
-					sf->vec_nr >> 3, 1 & (sf->vec_nr >> 2), 1 & (sf->vec_nr >> 1));
+	int error_code = sf->err_code;
+	color_printk(RED,BLACK,"do_invalid_TSS(10),ERROR_CODE:%#018lx\n",error_code);
+
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
 
 }
 
-void excep_page_fault(stack_frame_s * sf)
+void excep_seg_not_pres(stack_frame_s * sf)
 {
+	int error_code = sf->err_code;
+	color_printk(RED,BLACK,"do_segment_not_present(11),ERROR_CODE:%#018lx\n",error_code);
 
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
+}
+
+void excep_stack_segfault(stack_frame_s * sf)
+{
+	int error_code = sf->err_code;
+	color_printk(RED,BLACK,"do_stack_segment_fault(12),ERROR_CODE:%#018lx\n",error_code);
+
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
 }
 
 void excep_gen_prot(stack_frame_s * sf)
@@ -40,27 +95,67 @@ void excep_gen_prot(stack_frame_s * sf)
 	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
 }
 
+void excep_page_fault(stack_frame_s * sf)
+{
+	int error_code = sf->err_code;
+	unsigned long cr2 = 0;
+
+	__asm__	__volatile__("movq	%%cr2,	%0":"=r"(cr2)::"memory");
+
+	color_printk(RED,BLACK,"do_page_fault(14),ERROR_CODE:%#018lx\n",error_code);
+
+	if(!(error_code & 0x01))
+		color_printk(RED,BLACK,"Page Not-Present,\t");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Write Cause Fault,\t");
+	else
+		color_printk(RED,BLACK,"Read Cause Fault,\t");
+
+	if(error_code & 0x04)
+		color_printk(RED,BLACK,"Fault in user(3)\t");
+	else
+		color_printk(RED,BLACK,"Fault in supervisor(0,1,2)\t");
+
+	if(error_code & 0x08)
+		color_printk(RED,BLACK,",Reserved Bit Cause Fault\t");
+
+	if(error_code & 0x10)
+		color_printk(RED,BLACK,",Instruction fetch Cause Fault");
+
+	color_printk(RED,BLACK,"\n");
+
+	color_printk(RED,BLACK,"CR2:%#018lx\n",cr2);
+}
+
 void excep_intr_c_entry(stack_frame_s * sf)
 {
-		color_printk(WHITE, BLUE,"INTR: 0x%02x - %s \n", sf->vec_nr, intr_name[sf->vec_nr]);
-		if(sf->vec_nr < 32)
+	stack_frame_s * tmp = sf;
+	color_printk(WHITE, BLUE,"INTR: 0x%02x - %s \n", sf->vec_nr, intr_name[sf->vec_nr]);
+	if(sf->vec_nr < 32)
+	{
+		switch (sf->vec_nr)
 		{
-			switch (sf->vec_nr)
-			{
-			case INVAL_TSS_VEC:
-				excep_inval_tss(sf);
-				break;
-			case PAGE_FAULT_VEC:
-				excep_page_fault(sf);
-				break;
-			case GEN_PROT_VEC:
-				excep_gen_prot(sf);
-				break;
-			
-			default:
-				break;
-			}
-			while (1);
+		case INVAL_TSS_VEC:
+			excep_inval_tss(sf);
+			break;
+		case SEG_NOT_PRES_VEC:
+			excep_seg_not_pres(sf);
+			break;
+		case STACK_SEGFAULT_VEC:
+			excep_stack_segfault(sf);
+			break;
+		case GEN_PROT_VEC:
+			excep_gen_prot(sf);
+			break;
+		case PAGE_FAULT_VEC:
+			excep_page_fault(sf);
+			break;
+		
+		default:
+			break;
 		}
-		outb(INT_CTL, END_OF_INT);
+		while (1);
+	}
+	outb(INT_CTL, END_OF_INT);
 }
