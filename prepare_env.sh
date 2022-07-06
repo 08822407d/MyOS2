0a624e0ce8d9d125b8ddd4c8070aba8fb5cf2c31
08822407d <08822407d@gmail.com>
2022-07-06 11:46:01 +0800

    backup

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/bash
 
+ZBOOK="$HOME/vmware/grub2-test/grub2-test-flat.vmdk"
+HOME_ASUS="$HOME/vmware/myos2test/myos2test-flat.vmdk"
+
 source ./scripts/map_vdisk.sh
 
-main ~/vmware/myos2test/myos2test-flat.vmdk
+if [ $1 == "zbook" ]; then
+	main $ZBOOK
+elif [ $1 == "asus" ]; then
+	main $HOME_ASUS
+fi
