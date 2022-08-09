8ba396b57b02655d1870b8c68519969635fb1c52
08822407d <08822407d@gmail.com>
2022-08-09 17:54:58 +0800

    working on myglibc

--- mylib/myglibc/include/features.h
+++ mylib/myglibc/include/features.h
@@ -1,22 +1,22 @@
-// /* Copyright (C) 1991-2021 Free Software Foundation, Inc.
-//    This file is part of the GNU C Library.
+/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
 
-//    The GNU C Library is free software; you can redistribute it and/or
-//    modify it under the terms of the GNU Lesser General Public
-//    License as published by the Free Software Foundation; either
-//    version 2.1 of the License, or (at your option) any later version.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-//    The GNU C Library is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//    Lesser General Public License for more details.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-//    You should have received a copy of the GNU Lesser General Public
-//    License along with the GNU C Library; if not, see
-//    <https://www.gnu.org/licenses/>.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
 
-// #ifndef	_FEATURES_H
-// #define	_FEATURES_H	1
+#ifndef	_FEATURES_H
+#define	_FEATURES_H	1
 
 // /* These are defined by the user (or the compiler)
 //    to specify the desired environment:
@@ -512,4 +512,4 @@
 // #include <gnu/stubs.h>
 
 
-// #endif	/* features.h  */
+#endif	/* features.h  */
--- mylib/myglibc/sysdeps/unix/sysv_linux/opendir.c
+++ mylib/myglibc/sysdeps/unix/sysv_linux/opendir.c
@@ -1,12 +1,36 @@
+/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
 #include <dirent.h>
-#include <string.h>
-#include <stdlib.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <stdio.h>	/* For BUFSIZ.  */
+#include <sys/param.h>	/* For MIN and MAX.  */
+
+#include <not-cancel.h>
+
+
+#include <stdlib.h>
 #include <stdbool.h>
 
-#include <dirent.h>
 
-#include <fcntl.h>
+enum {
+	opendir_oflags = O_RDONLY|O_NDELAY|O_DIRECTORY|O_LARGEFILE|O_CLOEXEC
+};
 
 DIR *__alloc_dir(int fd, bool close_fd, int flags)
 {
