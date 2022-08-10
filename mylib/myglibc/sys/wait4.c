83281edb5953fd6db0e0eb25ff334d39b4236392
08822407d <08822407d@gmail.com>
2022-08-10 17:56:00 +0800

    working on myglibc

--- mylib/myglibc/sys/wait4.c
+++ mylib/myglibc/sys/wait4.c
@@ -1,6 +1,6 @@
-#include <sys/wait.h>
+#include <types.h>
 
-pid_t waitpid(pid_t pid, int *status, int options)
-{
-	return wait4(pid, status, options, NULL);
-}
+// pid_t waitpid(pid_t pid, int *status, int options)
+// {
+// 	return wait4(pid, status, options, NULL);
+// }
