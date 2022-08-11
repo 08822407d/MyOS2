9e119eb413ffaa5a3aa6849bb27b55fe254efc99
08822407d <08822407d@gmail.com>
2022-08-11 17:53:29 +0800

    working on myglibc

--- mylib/myglibc/string/memcmp.c
+++ mylib/myglibc/string/memcmp.c
@@ -3,8 +3,8 @@
 
 int memcmp(const void *x, const void *y, size_t n)
 {
-	const uint8_t* x8 = (const uint8_t*)x;
-	const uint8_t* y8 = (const uint8_t*)y;
+	const u_int8_t* x8 = (const u_int8_t*)x;
+	const u_int8_t* y8 = (const u_int8_t*)y;
 	size_t i;
 	for(i=0; i<n; i++) {
 		if(x8[i] < y8[i])
