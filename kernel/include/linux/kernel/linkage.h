e97c08d74f3784858bece6096d3b60bea736f38a
08822407d <08822407d@gmail.com>
2022-10-13 11:58:07 +0800

    working on memblock

--- kernel/include/linux/kernel/linkage.h
+++ kernel/include/linux/kernel/linkage.h
@@ -102,27 +102,27 @@
 		#define SYM_L_WEAK(name)	.weak name
 		#define SYM_L_LOCAL(name)	/* nothing */
 
-		// #ifndef LINKER_SCRIPT
-		// #define ALIGN __ALIGN
-		// #define ALIGN_STR __ALIGN_STR
+		#ifndef LINKER_SCRIPT
+			#define ALIGN __ALIGN
+			#define ALIGN_STR __ALIGN_STR
 
-		// /* === DEPRECATED annotations === */
+			// /* === DEPRECATED annotations === */
 
-		// #ifndef CONFIG_ARCH_USE_SYM_ANNOTATIONS
-		// #ifndef GLOBAL
-		// /* deprecated, use SYM_DATA*, SYM_ENTRY, or similar */
-		// #define GLOBAL(name)   \
-		// 	.globl name ASM_NL \
-		// 		name:
-		// #endif
+			// #ifndef CONFIG_ARCH_USE_SYM_ANNOTATIONS
+			// 	#ifndef GLOBAL
+			// 	/* deprecated, use SYM_DATA*, SYM_ENTRY, or similar */
+			// 	#	define GLOBAL(name)   \
+			// 				.globl name ASM_NL \
+			// 				name:
+			// 	#endif
 
-		// #ifndef ENTRY
-		// /* deprecated, use SYM_FUNC_START */
-		// #define ENTRY(name) \
-		// 	SYM_FUNC_START(name)
-		// #endif
-		// #endif /* CONFIG_ARCH_USE_SYM_ANNOTATIONS */
-		// #endif /* LINKER_SCRIPT */
+			// 	#ifndef ENTRY
+			// 	/* deprecated, use SYM_FUNC_START */
+			// 	#	define ENTRY(name) \
+			// 				SYM_FUNC_START(name)
+			// 	#endif
+			// #endif /* CONFIG_ARCH_USE_SYM_ANNOTATIONS */
+		#endif /* LINKER_SCRIPT */
 
 		// #ifndef CONFIG_ARCH_USE_SYM_ANNOTATIONS
 		// #ifndef WEAK
@@ -357,75 +357,6 @@
 		// 		data ASM_NL                   \
 		// 		SYM_DATA_END(name)
 		// #endif
-
-
-#ifdef __ELF__
-#define _C_LABEL(x) x
-#else
-#ifdef __STDC__
-#define _C_LABEL(x) _##x
-#else
-#define _C_LABEL(x) _ /**/ x
-#endif
-#endif
-#define _ASM_LABEL(x) x
-
-#ifdef __STDC__
-#define __CONCAT(x, y) x##y
-#define __STRING(x) #x
-#else
-#define __CONCAT(x, y) x /**/ y
-#define __STRING(x) "x"
-#endif
-
-/* let kernels and others override entrypoint alignment */
-#if !defined(_ALIGN_TEXT) && !defined(_KERNEL)
-#ifdef _STANDALONE
-#define _ALIGN_TEXT .align 1
-#elif defined __ELF__
-#define _ALIGN_TEXT .align 16
-#else
-#define _ALIGN_TEXT .align 4
-#endif
-#endif
-
-#define _ENTRY(x)       \
-	.text;              \
-	_ALIGN_TEXT;        \
-	.globl x;           \
-	.type x, @function; \
-	x:
-#define _LABEL(x) \
-	.globl x;     \
-	x:
-
-#ifdef GPROF
-#ifdef __ELF__
-#define _PROF_PROLOGUE      \
-	pushl % ebp;            \
-	movl % esp, % ebp;      \
-	call PIC_PLT(__mcount); \
-	popl % ebp
-#else
-#define _PROF_PROLOGUE    \
-	pushl % ebp;          \
-	movl % esp, % ebp;    \
-	call PIC_PLT(mcount); \
-	popl % ebp
-#endif
-#else
-#define _PROF_PROLOGUE
-#endif
-
-#define ENTRY(y)         \
-	_ENTRY(_C_LABEL(y)); \
-	_PROF_PROLOGUE
-#define NENTRY(y) _ENTRY(_C_LABEL(y))
-#define ASENTRY(y)         \
-	_ENTRY(_ASM_LABEL(y)); \
-	_PROF_PROLOGUE
-#define LABEL(y) _LABEL(_C_LABEL(y))
-#define END(y) .size y, .- y
 
 	#endif /* __ASSEMBLY__ */
 
