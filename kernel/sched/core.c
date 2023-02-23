79f26dbfa94d52d50ac37c65600f42a186f09c72
08822407d <08822407d@gmail.com>
2023-02-23 20:05:22 +0800

    workiong on kthread

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -404,43 +404,43 @@ int wake_up_process(task_s *p) {
  */
 void wake_up_new_task(task_s *p)
 {
-// 	struct rq_flags rf;
-// 	struct rq *rq;
+	// struct rq_flags rf;
+	// struct rq *rq;
 
-// 	raw_spin_lock_irqsave(&p->pi_lock, rf.flags);
+	// raw_spin_lock_irqsave(&p->pi_lock, rf.flags);
 	WRITE_ONCE(p->__state, TASK_RUNNING);
 // #ifdef CONFIG_SMP
-// 	/*
-// 	 * Fork balancing, do it here and not earlier because:
-// 	 *  - cpus_ptr can change in the fork path
-// 	 *  - any previously selected CPU might disappear through hotplug
-// 	 *
-// 	 * Use __set_task_cpu() to avoid calling sched_class::migrate_task_rq,
-// 	 * as we're not fully set-up yet.
-// 	 */
-// 	p->recent_used_cpu = task_cpu(p);
-// 	rseq_migrate(p);
-// 	__set_task_cpu(p, select_task_rq(p, task_cpu(p), WF_FORK));
+	// /*
+	//  * Fork balancing, do it here and not earlier because:
+	//  *  - cpus_ptr can change in the fork path
+	//  *  - any previously selected CPU might disappear through hotplug
+	//  *
+	//  * Use __set_task_cpu() to avoid calling sched_class::migrate_task_rq,
+	//  * as we're not fully set-up yet.
+	//  */
+	// p->recent_used_cpu = task_cpu(p);
+	// rseq_migrate(p);
+	// __set_task_cpu(p, select_task_rq(p, task_cpu(p), WF_FORK));
 // #endif
-// 	rq = __task_rq_lock(p, &rf);
-// 	update_rq_clock(rq);
-// 	post_init_entity_util_avg(p);
+	// rq = __task_rq_lock(p, &rf);
+	// update_rq_clock(rq);
+	// post_init_entity_util_avg(p);
 
-// 	activate_task(rq, p, ENQUEUE_NOCLOCK);
-// 	trace_sched_wakeup_new(p);
-// 	check_preempt_curr(rq, p, WF_FORK);
+	// activate_task(rq, p, ENQUEUE_NOCLOCK);
+	// trace_sched_wakeup_new(p);
+	// check_preempt_curr(rq, p, WF_FORK);
 // #ifdef CONFIG_SMP
-// 	if (p->sched_class->task_woken) {
-// 		/*
-// 		 * Nothing relies on rq->lock after this, so it's fine to
-// 		 * drop it.
-// 		 */
-// 		rq_unpin_lock(rq, &rf);
-// 		p->sched_class->task_woken(rq, p);
-// 		rq_repin_lock(rq, &rf);
-// 	}
+	// if (p->sched_class->task_woken) {
+	// 	/*
+	// 	 * Nothing relies on rq->lock after this, so it's fine to
+	// 	 * drop it.
+	// 	 */
+	// 	rq_unpin_lock(rq, &rf);
+	// 	p->sched_class->task_woken(rq, p);
+	// 	rq_repin_lock(rq, &rf);
+	// }
 // #endif
-// 	task_rq_unlock(rq, p, &rf);
+	// task_rq_unlock(rq, p, &rf);
 }
 
 /**
