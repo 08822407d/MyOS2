7c916876a0cd22955399430ddeb25c6f3f6fabce
08822407d <08822407d@gmail.com>
2023-04-26 12:08:09 +0800

    working on mmap() things

--- mykernel/include/linux/mm/mm_types.h
+++ mykernel/include/linux/mm/mm_types.h
@@ -31,6 +31,10 @@
 	typedef struct task_struct task_s;
 	// struct address_space;
 	// struct mem_cgroup;
+	struct mm_struct;
+	typedef struct mm_struct mm_s;
+	struct vm_area_struct;
+	typedef struct vm_area_struct vma_s;
 
 	/*
 	 * Each physical page in the system has a page_s associated with
@@ -424,84 +428,82 @@
 	// 	char name[];
 	// };
 
-	// /*
-	// * This struct describes a virtual memory area. There is one of these
-	// * per VM-area/task. A VM area is any part of the process virtual memory
-	// * space that has a special rule for the page-fault handlers (ie a shared
-	// * library, the executable area etc).
-	// */
-	// struct vm_area_struct
-	// {
-	// 	/* The first cache line has the info for VMA tree walking. */
+	/*
+	 * This struct describes a virtual memory area. There is one of these
+	 * per VM-area/task. A VM area is any part of the process virtual memory
+	 * space that has a special rule for the page-fault handlers (ie a shared
+	 * library, the executable area etc).
+	 */
+	typedef struct vm_area_struct {
+		/* The first cache line has the info for VMA tree walking. */
 
-	// 	unsigned long vm_start; /* Our start address within vm_mm. */
-	// 	unsigned long vm_end;	/* The first byte after our end address
-	// 				within vm_mm. */
+		unsigned long	vm_start;	/* Our start address within vm_mm. */
+		unsigned long	vm_end;		/* The first byte after our end address
+									   within vm_mm. */
 
-	// 	/* linked list of VM areas per task, sorted by address */
-	// 	struct vm_area_struct *vm_next, *vm_prev;
+		/* linked list of VM areas per task, sorted by address */
+		vma_s			*vm_next, *vm_prev;
 
-	// 	struct rb_node vm_rb;
+		// struct rb_node vm_rb;
 
-	// 	/*
-	// 	* Largest free memory gap in bytes to the left of this VMA.
-	// 	* Either between this VMA and vma->vm_prev, or between one of the
-	// 	* VMAs below us in the VMA rbtree and its ->vm_prev. This helps
-	// 	* get_unmapped_area find a free area of the right size.
-	// 	*/
-	// 	unsigned long rb_subtree_gap;
+		/*
+		* Largest free memory gap in bytes to the left of this VMA.
+		* Either between this VMA and vma->vm_prev, or between one of the
+		* VMAs below us in the VMA rbtree and its ->vm_prev. This helps
+		* get_unmapped_area find a free area of the right size.
+		*/
+		// unsigned long rb_subtree_gap;
 
-	// 	/* Second cache line starts here. */
+		/* Second cache line starts here. */
 
-	// 	struct mm_struct *vm_mm; /* The address space we belong to. */
+		mm_s			*vm_mm; /* The address space we belong to. */
 
-	// 	/*
-	// 	* Access permissions of this VMA.
-	// 	* See vmf_insert_mixed_prot() for discussion.
-	// 	*/
-	// 	pgprot_t vm_page_prot;
-	// 	unsigned long vm_flags; /* Flags, see mm.h. */
+		/*
+		* Access permissions of this VMA.
+		* See vmf_insert_mixed_prot() for discussion.
+		*/
+		// pgprot_t vm_page_prot;
+		unsigned long	vm_flags; /* Flags, see mm.h. */
 
-	// 	/*
-	// 	* For areas with an address space and backing store,
-	// 	* linkage into the address_space->i_mmap interval tree.
-	// 	*
-	// 	* For private anonymous mappings, a pointer to a null terminated string
-	// 	* containing the name given to the vma, or NULL if unnamed.
-	// 	*/
+		/*
+		 * For areas with an address space and backing store,
+		 * linkage into the address_space->i_mmap interval tree.
+		 *
+		 * For private anonymous mappings, a pointer to a null terminated string
+		 * containing the name given to the vma, or NULL if unnamed.
+		 */
 
-	// 	union
-	// 	{
-	// 		struct
-	// 		{
-	// 			struct rb_node rb;
-	// 			unsigned long rb_subtree_last;
-	// 		} shared;
-	// 		/*
-	// 		* Serialized by mmap_sem. Never use directly because it is
-	// 		* valid only when vm_file is NULL. Use anon_vma_name instead.
-	// 		*/
-	// 		struct anon_vma_name *anon_name;
-	// 	};
+		// union
+		// {
+		// 	struct
+		// 	{
+		// 		struct rb_node rb;
+		// 		unsigned long rb_subtree_last;
+		// 	} shared;
+		// 	/*
+		// 	* Serialized by mmap_sem. Never use directly because it is
+		// 	* valid only when vm_file is NULL. Use anon_vma_name instead.
+		// 	*/
+		// 	struct anon_vma_name *anon_name;
+		// };
 
-	// 	/*
-	// 	* A file's MAP_PRIVATE vma can be in both i_mmap tree and anon_vma
-	// 	* list, after a COW of one of the file pages.	A MAP_SHARED vma
-	// 	* can only be in the i_mmap tree.  An anonymous MAP_PRIVATE, stack
-	// 	* or brk vma (with NULL file) can only be in an anon_vma list.
-	// 	*/
-	// 	List_s anon_vma_chain; /* Serialized by mmap_lock &
-	// 									* page_table_lock */
-	// 	struct anon_vma *anon_vma;		 /* Serialized by page_table_lock */
+		// /*
+		// * A file's MAP_PRIVATE vma can be in both i_mmap tree and anon_vma
+		// * list, after a COW of one of the file pages.	A MAP_SHARED vma
+		// * can only be in the i_mmap tree.  An anonymous MAP_PRIVATE, stack
+		// * or brk vma (with NULL file) can only be in an anon_vma list.
+		// */
+		// List_s anon_vma_chain; /* Serialized by mmap_lock &
+		// 								* page_table_lock */
+		// struct anon_vma *anon_vma;		 /* Serialized by page_table_lock */
 
-	// 	/* Function pointers to deal with this struct. */
-	// 	const struct vm_operations_struct *vm_ops;
+		// /* Function pointers to deal with this struct. */
+		// const struct vm_operations_struct *vm_ops;
 
-	// 	/* Information about our backing store: */
-	// 	unsigned long vm_pgoff; /* Offset (within vm_file) in PAGE_SIZE
-	// 				units */
-	// 	file_s *vm_file;	/* File we map to (can be NULL). */
-	// 	void *vm_private_data;	/* was vm_pte (shared mem) */
+		/* Information about our backing store: */
+		unsigned long	vm_pgoff; /* Offset (within vm_file) in PAGE_SIZE units */
+		file_s			*vm_file;	/* File we map to (can be NULL). */
+		// void *vm_private_data;	/* was vm_pte (shared mem) */
 
 	// #ifdef CONFIG_SWAP
 	// 	atomic_long_t swap_readahead_info;
@@ -513,40 +515,40 @@
 	// 	struct mempolicy *vm_policy; /* NUMA policy for the VMA */
 	// #endif
 	// 	struct vm_userfaultfd_ctx vm_userfaultfd_ctx;
-	// };
+	} vma_s;
 
 	// struct kioctx_table;
 	typedef struct mm_struct {
 		struct
 		{
-	// 		struct vm_area_struct *mmap; /* list of VMAs */
-	// 		struct rb_root mm_rb;
-	// 		u64 vmacache_seqnum; /* per-thread vmacache */
+			vma_s			*mmap;				/* list of VMAs */
+			// struct rb_root mm_rb;
+			// u64 vmacache_seqnum; /* per-thread vmacache */
 	// #ifdef CONFIG_MMU
-	// 		unsigned long (*get_unmapped_area)(file_s *filp,
-	// 										unsigned long addr, unsigned long len,
-	// 										unsigned long pgoff, unsigned long flags);
+			// unsigned long (*get_unmapped_area)(file_s *filp,
+			// 								unsigned long addr, unsigned long len,
+			// 								unsigned long pgoff, unsigned long flags);
 	// #endif
-	// 		unsigned long mmap_base;		/* base of mmap area */
-	// 		unsigned long mmap_legacy_base; /* base of mmap area in bottom-up allocations */
+			unsigned long	mmap_base;			/* base of mmap area */
+			// unsigned long mmap_legacy_base; /* base of mmap area in bottom-up allocations */
 	// #ifdef CONFIG_HAVE_ARCH_COMPAT_MMAP_BASES
-	// 		/* Base addresses for compatible mmap() */
-	// 		unsigned long mmap_compat_base;
-	// 		unsigned long mmap_compat_legacy_base;
+			// /* Base addresses for compatible mmap() */
+			// unsigned long mmap_compat_base;
+			// unsigned long mmap_compat_legacy_base;
 	// #endif
-	// 		unsigned long task_size;	  /* size of task vm space */
-	// 		unsigned long highest_vm_end; /* highest vma end address */
+			unsigned long	task_size;			/* size of task vm space */
+			unsigned long	highest_vm_end;		/* highest vma end address */
 			// pgd_t	*pgd;
-			reg_t		pgd_ptr;
+			reg_t			pgd_ptr;
 
 	// #ifdef CONFIG_MEMBARRIER
-	// 		/**
-	// 		 * @membarrier_state: Flags controlling membarrier behavior.
-	// 		 *
-	// 		 * This field is close to @pgd to hopefully fit in the same
-	// 		 * cache-line, which needs to be touched by switch_mm().
-	// 		 */
-	// 		atomic_t membarrier_state;
+			// /**
+			//  * @membarrier_state: Flags controlling membarrier behavior.
+			//  *
+			//  * This field is close to @pgd to hopefully fit in the same
+			//  * cache-line, which needs to be touched by switch_mm().
+			//  */
+			// atomic_t membarrier_state;
 	// #endif
 
 			/**
@@ -558,7 +560,7 @@
 			 * @mm_count (which may then free the &struct mm_struct if
 			 * @mm_count also drops to 0).
 			 */
-			atomic_t mm_users;
+			atomic_t		mm_users;
 
 			/**
 			 * @mm_count: The number of references to &struct mm_struct
@@ -567,72 +569,71 @@
 			 * Use mmgrab()/mmdrop() to modify. When this drops to 0, the
 			 * &struct mm_struct is freed.
 			 */
-			atomic_t mm_refcount;
+			atomic_t		mm_refcount;
 
 	// #ifdef CONFIG_MMU
-	// 		atomic_long_t pgtables_bytes; /* PTE page table pages */
+			atomic_long_t	pgtables_bytes;		/* PTE page table pages */
 	// #endif
-	// 		int map_count; /* number of VMAs */
+			int				map_count;			/* number of VMAs */
 
-	// 		spinlock_t page_table_lock; /* Protects page tables and some
-	// 									* counters
-	// 									*/
-	// 		/*
-	// 		* With some kernel config, the current mmap_lock's offset
-	// 		* inside 'mm_struct' is at 0x120, which is very optimal, as
-	// 		* its two hot fields 'count' and 'owner' sit in 2 different
-	// 		* cachelines,  and when mmap_lock is highly contended, both
-	// 		* of the 2 fields will be accessed frequently, current layout
-	// 		* will help to reduce cache bouncing.
-	// 		*
-	// 		* So please be careful with adding new fields before
-	// 		* mmap_lock, which can easily push the 2 fields into one
-	// 		* cacheline.
-	// 		*/
-	// 		struct rw_semaphore mmap_lock;
+			spinlock_t		page_table_lock;	/* Protects page tables and some
+												   counters */
+			// /*
+			// * With some kernel config, the current mmap_lock's offset
+			// * inside 'mm_struct' is at 0x120, which is very optimal, as
+			// * its two hot fields 'count' and 'owner' sit in 2 different
+			// * cachelines,  and when mmap_lock is highly contended, both
+			// * of the 2 fields will be accessed frequently, current layout
+			// * will help to reduce cache bouncing.
+			// *
+			// * So please be careful with adding new fields before
+			// * mmap_lock, which can easily push the 2 fields into one
+			// * cacheline.
+			// */
+			// struct rw_semaphore mmap_lock;
 
-	// 		List_s mmlist; /* List of maybe swapped mm's.	These
-	// 								* are globally strung together off
-	// 								* init_mm.mmlist, and are protected
-	// 								* by mmlist_lock
-	// 								*/
+			// List_s mmlist; /* List of maybe swapped mm's.	These
+			// 						* are globally strung together off
+			// 						* init_mm.mmlist, and are protected
+			// 						* by mmlist_lock
+			// 						*/
 
-	// 		unsigned long hiwater_rss; /* High-watermark of RSS usage */
-	// 		unsigned long hiwater_vm;  /* High-water virtual memory usage */
+			// unsigned long hiwater_rss; /* High-watermark of RSS usage */
+			// unsigned long hiwater_vm;  /* High-water virtual memory usage */
 
-	// 		unsigned long total_vm;	 /* Total pages mapped */
-	// 		unsigned long locked_vm; /* Pages that have PG_mlocked set */
-	// 		atomic64_t pinned_vm;	 /* Refcount permanently increased */
-	// 		unsigned long data_vm;	 /* VM_WRITE & ~VM_SHARED & ~VM_STACK */
-	// 		unsigned long exec_vm;	 /* VM_EXEC & ~VM_WRITE & ~VM_STACK */
-	// 		unsigned long stack_vm;	 /* VM_STACK */
+			// unsigned long total_vm;	 /* Total pages mapped */
+			// unsigned long locked_vm; /* Pages that have PG_mlocked set */
+			// atomic64_t pinned_vm;	 /* Refcount permanently increased */
+			// unsigned long data_vm;	 /* VM_WRITE & ~VM_SHARED & ~VM_STACK */
+			// unsigned long exec_vm;	 /* VM_EXEC & ~VM_WRITE & ~VM_STACK */
+			// unsigned long stack_vm;	 /* VM_STACK */
 			// unsigned long def_flags;
 
-	// 		/**
-	// 		 * @write_protect_seq: Locked when any thread is write
-	// 		 * protecting pages mapped by this mm to enforce a later COW,
-	// 		 * for instance during page table copying for fork().
-	// 		 */
-	// 		seqcount_t write_protect_seq;
+			// /**
+			//  * @write_protect_seq: Locked when any thread is write
+			//  * protecting pages mapped by this mm to enforce a later COW,
+			//  * for instance during page table copying for fork().
+			//  */
+			// seqcount_t write_protect_seq;
 
-	// 		spinlock_t arg_lock; /* protect the below fields */
+			// spinlock_t arg_lock; /* protect the below fields */
 
 			unsigned long	start_code, end_code, start_data, end_data;
 			unsigned long	start_brk, brk, start_stack;
 			unsigned long	arg_start, arg_end, env_start, env_end;
 
-	// 		unsigned long saved_auxv[AT_VECTOR_SIZE]; /* for /proc/PID/auxv */
+			// unsigned long saved_auxv[AT_VECTOR_SIZE]; /* for /proc/PID/auxv */
 
-	// 		/*
-	// 		* Special counters, in some configurations protected by the
-	// 		* page_table_lock, in other configurations by being atomic.
-	// 		*/
-	// 		struct mm_rss_stat rss_stat;
+			// /*
+			// * Special counters, in some configurations protected by the
+			// * page_table_lock, in other configurations by being atomic.
+			// */
+			// struct mm_rss_stat rss_stat;
 
-	// 		struct linux_binfmt *binfmt;
+			// struct linux_binfmt *binfmt;
 
-	// 		/* Architecture-specific MM context */
-	// 		mm_context_t context;
+			// /* Architecture-specific MM context */
+			// mm_context_t context;
 
 			unsigned long	flags; /* Must use atomic bitops to access */
 
@@ -656,7 +657,7 @@
 	// 		struct user_namespace *user_ns;
 
 			/* store ref to file /proc/<pid>/exe symlink points to */
-			file_s __rcu *exe_file;
+			file_s __rcu	*exe_file;
 	// #ifdef CONFIG_MMU_NOTIFIER
 	// 		struct mmu_notifier_subscriptions *notifier_subscriptions;
 	// #endif
