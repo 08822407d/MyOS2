0bda7ce04be3509d76887393f2414d013deaee87
08822407d <08822407d@gmail.com>
2024-01-11 18:48:19 +0800

    working on xhci init

--- mykernel/arch/x86_64/myos/myos_XHCI_TRB.h
+++ mykernel/arch/x86_64/myos/myos_XHCI_TRB.h
@@ -45,6 +45,21 @@
 		u32		Fields[4];
 	} XHCI_TRB_s;
 
+	typedef struct XHCI_TransferTRB_Common
+	{
+		u32		Fields1[2];
+
+		u32		Fields2			: 22,
+				Intr_Target		: 10;
+
+		u32		Cycle_Bit		: 1,
+				Fields3			: 5,
+				Immediate_Data	: 1,
+				Fields4			: 3,
+				TRB_Type		: 6,
+				Fields5			: 16;
+	} __attribute__((packed)) XHCI_TransTRB_s;
+	
 	typedef struct XHCI_EventTRB_Common {
 		u32		Fields1[3];
 
@@ -64,19 +79,141 @@
 				Slot_ID			: 8;
 	} __attribute__((packed)) XHCI_CmdTRB_s;
 
+	typedef struct XHCI_OtherTRB_Common
+	{
+		u64		Fields1;
+
+		u32		RsvdZ1			: 22,
+				Intr_Target		: 10;
+
+		u32		Cycle_Bit		: 1,
+				Fields2			: 1,
+				RsvdZ2			: 2,
+				Chain_Bit		: 1,
+				IntrOnComp		: 1,
+				Fields3			: 4,
+				TRB_Type		: 6,
+				RsvdZ3			: 16;
+	} __attribute__((packed)) XHCI_OtherTRB_s;
+
+
+/*==============================================================================================*
+ *											transfer class										*
+ *==============================================================================================*/
+	typedef struct XHCI_Setup_Stage_TRB
+	{
+		u8		bmRequestType;
+		u8		bRequest;
+		u16		wValue;
+		u16		wIndex;
+		u16		wLength;
+
+		u32		TRB_TransLeng	: 17,		
+				RsvdZ1			: 5,
+				Intr_Target		: 10;
+
+		u32		Cycle_Bit		: 1,
+				RsvdZ2			: 4,
+				IntrOnComp		: 1,
+				Immediate_Data	: 1,
+				RsvdZ3			: 3,
+				TRB_Type		: 6,
+				Trans_Type		: 2,
+				RsvdZ4			: 14;
+	} __attribute__((packed)) XHCI_SetupStage_s;
+
+	typedef struct XHCI_Data_Stage_TRB
+	{
+		u64		Data_BUffer;
+
+		u32		TRB_TransLeng	: 17,		
+				TD_Size			: 5,
+				Intr_Target		: 10;
+
+		u32		Cycle_Bit		: 1,
+				Eval_Next_TRB	: 1,
+				ISP				: 1,
+				No_Snoop		: 1,
+				Chain_Bit		: 1,
+				IntrOnComp		: 1,
+				Immediate_Data	: 1,
+				RsvdZ1			: 3,
+				TRB_Type		: 6,
+				Direction		: 1,
+				RsvdZ2			: 15;
+	} __attribute__((packed)) XHCI_DataStage_s;
+
+	typedef struct XHCI_Statue_Stage_TRB
+	{
+		u64		RsvdZ1;
+
+		u32		RsvdZ2			: 22,		
+				Intr_Target		: 10;
+
+		u32		Cycle_Bit		: 1,
+				Eval_Next_TRB	: 1,
+				RsvdZ3			: 2,
+				Chain_Bit		: 1,
+				IntrOnComp		: 1,
+				RsvdZ4			: 4,
+				TRB_Type		: 6,
+				Direction		: 1,
+				RsvdZ5			: 15;
+	} __attribute__((packed)) XHCI_StatusStage_s;
+
+
+/*==============================================================================================*
+ *											event class											*
+ *==============================================================================================*/
+	typedef struct XHCI_Transfer_Event_TRB
+	{
+		u64		TRB_ptr;
+
+		u32		TRB_TransLeng	: 24,		
+				Compl_Code		: 8;
+
+		u32		Cycle_Bit		: 1,
+				RsvdZ1			: 1,
+				Event_Data		: 1,
+				RsvdZ2			: 7,
+				TRB_Type		: 6,
+				EndPt_ID		: 5,
+				RsvdZ3			: 3,
+				Slot_ID			: 8;
+	} __attribute__((packed)) XHCI_TransEvt_s;
+
 	typedef struct XHCI_Command_Completion_Event_TRB {
 		u64		CmdTRB_ptr;
 
 		u32		CmdComp_Param	: 24,
-				CmdCode			: 8;
+				Compl_Code		: 8;
 
 		u32     Cycle_Bit		: 1,
 				RsvdZ			: 9,
 				TRB_Type		: 6,
 				VF_ID			: 8,
 				Slot_ID			: 8;
-	} __attribute__((packed)) XHCI_CCE_TRB_s;
+	} __attribute__((packed)) XHCI_CmdComplEvt_TRB_s;
 
+	typedef struct XHCI_Status_Change_Event_TRB {
+		u32		RsvdZ1			: 24,
+				Port_ID			: 8;
+
+		u32		RsvdZ2;
+
+		u32		RsvdZ3			: 24,
+				Compl_Code		: 8;
+
+		u32     Cycle_Bit		: 1,
+				RsvdZ4			: 9,
+				TRB_Type		: 6,
+				RsvdZ5			: 16;
+	} __attribute__((packed)) XHCI_StChEvt_TRB_s;
+
+
+/*==============================================================================================*
+ *											command class										*
+ *==============================================================================================*/
 	typedef struct XHCI_No_Op_Command_TRB {
 		u32		RsvdZ1[3];
 
@@ -87,6 +224,10 @@
 				RsvdZ2			: 8;
 	} __attribute__((packed)) XHCI_NoOpCmd_TRB_s;
 
+
+/*==============================================================================================*
+ *											other class											*
+ *==============================================================================================*/
 	typedef struct XHCI_Link_TRB {
 		u64		CmdTRB_ptr;
 		
@@ -102,5 +243,22 @@
 				TRB_Type		: 6,
 				RsvdZ4			: 16;
 	} __attribute__((packed)) XHCI_LinkTRB_s;
+
+	typedef struct XHCI_EventData_TRB {
+		u64		CmdTRB_ptr;
+		
+		u32		RsvdZ1			: 22,
+				Intr_Target		: 10;
+
+		u32		Cycle_Bit		: 1,
+				Eval_Next_TRB	: 1,
+				RsvdZ2			: 2,
+				Chain_Bit		: 1,
+				IntrOnComp		: 1,
+				RsvdZ3			: 3,
+				Blk_Evt_Intr	: 1,
+				TRB_Type		: 6,
+				RsvdZ4			: 16;
+	} __attribute__((packed)) XHCI_EvtDataTRB_s;
 
 #endif
